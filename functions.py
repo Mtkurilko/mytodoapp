0f8255da1c894e07487fb0292a7ff0460bb8bfd8
mtk2024 <drmichael1519@gmail.com>
2024-06-15 11:24:09 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_local, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_local)
